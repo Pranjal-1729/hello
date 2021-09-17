f69170ca6985fdcf65a9378ec8239475d1e4f608
Pranjal-1729 <49397202+Pranjal-1729@users.noreply.github.com>
2021-09-17 11:17:38 +0530

    Create Hello1.java

---
+++ Hello1.java
@@ -0,0 +1,7 @@
+public class Hello1 {
+    public static void main(String[] args) {
+        System.out.println("Hello world from git code");
+        System.out.println("Hello world from git code");
+        System.out.println("This is a new commit done..");
+    }
+}
